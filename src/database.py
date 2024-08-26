8a9dbd68fc965bff646380dd3006bb1a29c66481
ben05allen <ben05allen@hotmail.com>
2024-08-26 12:00:43 +0900

    adding typing

--- src/database.py
+++ src/database.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 import os
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 load_dotenv()
 CONN_STRING = os.environ["CONN_STR"]
 
 
-async def get_session():
+async def get_session() -> async_sessionmaker[AsyncSession]:
     engine = create_async_engine(CONN_STRING, echo=True)
     return async_sessionmaker(engine, expire_on_commit=False)
